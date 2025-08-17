be3566c2de408b3095b998eb04a8c9f44a284f06
ehzilehzil <ezhilehzil@outlook.com>
2025-08-17 21:53:11 +0900

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/mapping_tool/', // GitHub Pages에서 사용하는 서브 디렉토리 경로
   vite: {
     plugins: [tailwindcss()]
   }
